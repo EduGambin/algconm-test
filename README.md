280838dc7c0950af717469cf39e924d32686e4b2
Edu Gambín <gambinmonserratedu@gmail.com>
2023-06-27 11:29:56 +0200

    app added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test de álgebra conmutativa
+
+Veamos si llevas bien la asignatura con este tipo test de cuatro opciones. Necesitas Python3 para ejecutarlo, pero si estás en Linux puedes lanzarlo con `./test.py`. En Windows puede que funcione también, pero no lo he probado.
+
+Cualquier error por favor avisadme para que lo corrija. Si se os ocurre alguna pregunta más sentíos libres de hacer un pull request para que la añada.
+
+Experiencia estándar estudiando esta asignatura:
+
+<p align="center">
+<img src="https://preview.redd.it/the-linear-algebra-experience-v0-c14d4l8ag17b1.jpg?width=640&crop=smart&auto=webp&s=4de0343d29460f2190fb6b94edb2a6a60799b8d1" alt="Leap of logic" width="50%"/><br>Fuente: <a href="https://www.reddit.com/r/mathmemes/comments/14dqgbz/the_linear_algebra_experience/">Reddit</a>
+</p>
---
+++ test.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import random
+import csv
+
+
+# Read the questions from the "questions.csv" file using the csv module
+def read_questions():
+    with open('questions.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header row
+        questions = list(reader)
+    return questions
+
+
+# The CSV contains a list of questions and answers in the following format:
+# question,answer,fake1,fake2,fake3
+
+def main():
+    questions = read_questions()
+
+    print("Elige una opción:")
+    print("  1. 10 preguntas aleatorias")
+    print("  2. 20 preguntas aleatorias")
+    print("  3. 30 preguntas aleatorias")
+    print("  4. Todas las preguntas")
+    print("  5. Salir")
+    print()
+
+    option = int(input("Opción: "))
+
+    if option == 1:
+        num_questions = 10
+    elif option == 2:
+        num_questions = 20
+    elif option == 3:
+        num_questions = 30
+    elif option == 4:
+        num_questions = len(questions)
+    elif option == 5:
+        exit()
+    else:
+        print("Opción incorrecta")
+        exit()
+
+    print("Seleccionadas {} preguntas".format(num_questions))
+    print("-" * 80)
+
+    correct_answers = 0
+    failed_answers = []
+
+    # Shuffle the questions
+    random.shuffle(questions)
+
+    questions = questions[:num_questions]
+
+    for j, q in enumerate(questions):
+        # Make room
+        print()
+
+        # Print the question in blue and bold
+        print('\033[1;34m{}. {}\033[0m'.format(j + 1, q[0]))
+
+        # Shuffle the answers but remember the correct answer
+        answers = [q[1], q[2], q[3], q[4]]
+        random.shuffle(answers)
+        correct_answer = answers.index(q[1])
+
+        # Print the answers
+        for i, a in enumerate(answers):
+            print('  {}. {}'.format(i + 1, a))
+        print()
+
+        # Get the user's answer
+        user_answer = int(input('Respuesta: '))
+        if user_answer == correct_answer + 1:
+            print('\033[1;32m¡Correcto!\033[0m')
+            correct_answers += 1
+        else:
+            print('\033[1;31m¡Incorrecto!\033[0m')
+            print('La respuesta correcta es la {}.'.format(correct_answer + 1))
+            failed_answers.append(j+1)
+
+    print()
+    print('Has acertado {} preguntas de {}.'.format(
+        correct_answers, len(questions)))
+    if failed_answers:
+        print('Preguntas falladas: {}'.format(
+            ', '.join(map(str, failed_answers))))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Saliendo...')
+        exit()
