f7089ececf10f1123d1a1caf5a9ee1e669c5bec3
Edu Gambín <gambinmonserratedu@gmail.com>
2023-06-27 12:14:31 +0200

    minor fixes and qof upgrades

--- test.py
+++ test.py
@@ -13,12 +13,20 @@ def read_questions():
     return questions
 
 
+correct_answers = 0
+failed_answers = []
+questions = read_questions()
+
+
 # The CSV contains a list of questions and answers in the following format:
 # question,answer,fake1,fake2,fake3
 
 def main():
-    questions = read_questions()
+    global correct_answers
+    global failed_answers
+    global questions
 
+    print("-" * 80)
     print("Elige una opción:")
     print("  1. 10 preguntas aleatorias")
     print("  2. 20 preguntas aleatorias")
@@ -27,7 +35,14 @@ def main():
     print("  5. Salir")
     print()
 
-    option = int(input("Opción: "))
+    while True:
+        try:
+            option = int(input("Opción: "))
+            if option < 1 or option > 5:
+                raise ValueError
+            break
+        except ValueError:
+            print('\033[1;31mIntroduce un número entre 1 y 5.\033[0m')
 
     if option == 1:
         num_questions = 10
@@ -37,17 +52,11 @@ def main():
         num_questions = 30
     elif option == 4:
         num_questions = len(questions)
-    elif option == 5:
-        exit()
     else:
-        print("Opción incorrecta")
         exit()
 
     print("Seleccionadas {} preguntas".format(num_questions))
     print("-" * 80)
-
-    correct_answers = 0
-    failed_answers = []
 
     # Shuffle the questions
     random.shuffle(questions)
@@ -64,6 +73,7 @@ def main():
         # Shuffle the answers but remember the correct answer
         answers = [q[1], q[2], q[3], q[4]]
         random.shuffle(answers)
+
         correct_answer = answers.index(q[1])
 
         # Print the answers
@@ -71,8 +81,17 @@ def main():
             print('  {}. {}'.format(i + 1, a))
         print()
 
-        # Get the user's answer
-        user_answer = int(input('Respuesta: '))
+        # Get the user's answer. If it's not a number or it's not between 1 and
+        # 4, ask again
+        while True:
+            try:
+                user_answer = int(input('Respuesta: '))
+                if user_answer < 1 or user_answer > 4:
+                    raise ValueError
+                break
+            except ValueError:
+                print('Por favor, introduce un número entre 1 y 4.')
+
         if user_answer == correct_answer + 1:
             print('\033[1;32m¡Correcto!\033[0m')
             correct_answers += 1
@@ -81,18 +100,17 @@ def main():
             print('La respuesta correcta es la {}.'.format(correct_answer + 1))
             failed_answers.append(j+1)
 
-    print()
-    print('Has acertado {} preguntas de {}.'.format(
-        correct_answers, len(questions)))
-    if failed_answers:
-        print('Preguntas falladas: {}'.format(
-            ', '.join(map(str, failed_answers))))
-
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         print()
-        print('Saliendo...')
-        exit()
+        print('Terminado por el usuario.')
+
+    print()
+    print('Has acertado {} preguntas de {}.'.format(
+        correct_answers, len(questions)))
+    if failed_answers:
+        print('Preguntas falladas: {}'.format(
+            ', '.join(map(str, failed_answers))))
